05bd17fab48191e1db6dbd70c3f99de0fa6f3933
syunta <kitagawa@timedia.co.jp>
2016-07-26 23:17:31 +0900

    Implement Show instance

--- scheme.hs
+++ scheme.hs
@@ -1,4 +1,9 @@
 data SObj = SInt Int |
             Cell (SObj, SObj) |
             Nil
-            deriving (Show)
+
+instance Show SObj where
+  show (SInt x) = show x
+  show (Cell (x, Nil)) = "(" ++ show x ++ ")"
+  show (Cell (x, y)) = "(" ++ show x ++ " " ++ show y ++ ")"
+  show Nil = ""
