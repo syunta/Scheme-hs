23cfce1f9b41eb650cd3eeaf172dbc0c8d512ce8
syunta <kitagawa@timedia.co.jp>
2016-07-29 15:02:13 +0900

    Add SDotList data

--- Types.hs
+++ Types.hs
@@ -7,9 +7,11 @@ import Data.List
 
 data SObj = SInt Int |
             SList [SObj] |
+            SDotList [SObj] SObj |
             Nil
 
 instance Show SObj where
   show (SInt x) = show x
   show (SList xs) = "(" ++ ((intercalate " ") . (map show) $ xs) ++ ")"
+  show (SDotList xs y) = "(" ++ ((intercalate " ") . (map show) $ xs) ++ " . " ++ show y ++ ")"
   show Nil = ""
