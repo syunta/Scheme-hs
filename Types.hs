4325abaa039b6bf02a28ba5e406e61f76e81b7c4
syunta <kitagawa@timedia.co.jp>
2016-07-29 15:05:38 +0900

    Remove uneeded Nil

--- Types.hs
+++ Types.hs
@@ -7,11 +7,9 @@ import Data.List
 
 data SObj = SInt Int |
             SList [SObj] |
-            SDotList [SObj] SObj |
-            Nil
+            SDotList [SObj] SObj
 
 instance Show SObj where
   show (SInt x) = show x
   show (SList xs) = "(" ++ ((intercalate " ") . (map show) $ xs) ++ ")"
   show (SDotList xs y) = "(" ++ ((intercalate " ") . (map show) $ xs) ++ " . " ++ show y ++ ")"
-  show Nil = ""
