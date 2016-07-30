7e693ecc428e2e37d9992bac7740cb4c3e3edc40
syunta <kitagawa@timedia.co.jp>
2016-07-30 20:35:19 +0900

    Add parseList

--- Parser.hs
+++ Parser.hs
@@ -13,6 +13,16 @@ separate ts =
       t ("", ys) = [[head ys]] ++ separate (tail ys)
       t (xs, ys) = [xs, [head ys]] ++ separate (tail ys)
 
+parseList :: (SObj, [String]) -> (SObj, [String])
+parseList (exps, ")":tokens) = (exps, tokens)
+parseList (exps, "(":tokens) =
+  let (sls, restTokens) = parseList (SList [], tokens)
+      (SList ls, restRestTokens) = parseList (exps, restTokens) in
+  (SList (sls : ls), restRestTokens)
+parseList (exps, token:tokens) =
+  let (SList ls, restTokens) = parseList (exps, tokens) in
+  (SList (parseAtom token : ls), restTokens)
+
 parseAtom :: String -> SObj
 parseAtom token
   | not . (Nothing ==) $ mval = let (Just val) = mval in (SInt val)
