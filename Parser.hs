1dd2861337d6657157e1beadd36ad89655ff8c84
syunta <kitagawa@timedia.co.jp>
2016-07-30 19:09:40 +0900

    Add parseAtom

--- Parser.hs
+++ Parser.hs
@@ -1,4 +1,5 @@
 import Types
+import Text.Read
 
 tokenize :: String -> [String]
 tokenize = concat . (map separate) . words
@@ -11,3 +12,8 @@ separate ts =
       t (xs, "") = [xs]
       t ("", ys) = [[head ys]] ++ separate (tail ys)
       t (xs, ys) = [xs, [head ys]] ++ separate (tail ys)
+
+parseAtom :: String -> SObj
+parseAtom token
+  | not . (Nothing ==) $ mval = let (Just val) = mval in (SInt val)
+    where mval = (readMaybe token :: Maybe Int)
