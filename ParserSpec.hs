c6d1651742ee85d6202412f6b1e3ef4fe94f9c95
syunta <kitagawa@timedia.co.jp>
2017-03-21 20:18:08 +0900

    Add spec for empty list

--- ParserSpec.hs
+++ ParserSpec.hs
@@ -9,6 +9,8 @@ main = hspec $ do
       parse "(1)" `shouldBe` (SList [SInt 1] Nil, [])
       parse "(1 2)" `shouldBe` (SList [SInt 1, SInt 2] Nil, [])
       parse "(1 (2 3))" `shouldBe` (SList [SInt 1, SList [SInt 2, SInt 3] Nil] Nil, [])
+    it "parses empty list to sobj" $ do
+      parse "()" `shouldBe` (SList [] Nil, [])
     it "parses dotted list to sobj" $ do
       parse "(1 . 2)" `shouldBe` (SList [SInt 1] (SInt 2), [])
     it "parses number to sobj" $ do
