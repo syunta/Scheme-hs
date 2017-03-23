2832c9967124ffa542291326291c6a8ac640334e
syunta <kitagawa@timedia.co.jp>
2017-03-23 20:01:50 +0900

    Add primPlus

--- Subr.hs
+++ Subr.hs
@@ -6,4 +6,10 @@ module Subr
 import Types
 
 primitiveProcedures :: [(String, [SObj] -> SObj)]
-primitiveProcedures = []
+primitiveProcedures = [("+", primPlus)]
+
+primPlus :: [SObj] -> SObj
+primPlus args = foldl plus (SInt 0) args
+
+plus :: SObj -> SObj -> SObj
+plus (SInt x) (SInt y) = SInt (x + y)
