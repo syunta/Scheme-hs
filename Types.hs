7cb28d8b5e4059a6efb1874270443e04c5629a6f
syunta <kitagawa@timedia.co.jp>
2017-03-23 20:19:14 +0900

    Fix Promitive to have function

--- Types.hs
+++ Types.hs
@@ -11,8 +11,7 @@ data SObj = SInt Int |
             SSymbol String |
             SList [SObj] SObj |
             Nil |
-            Primitive String
-            deriving Eq
+            Primitive String ([SObj] -> SObj)
 
 type Env = [Frame]
 type Frame = ([String], [SObj])
@@ -20,7 +19,7 @@ type Frame = ([String], [SObj])
 instance Show SObj where
   show (SSymbol x) = x
   show (SInt x) = show x
-  show (Primitive x) = show "<subr " ++ x ++ " >"
+  show (Primitive x _) = show "<subr " ++ x ++ " >"
   show (SList ((SSymbol "quote"):xs) Nil) = showElements xs
   show (SList xs Nil) = "(" ++ (showElements xs) ++ ")"
   show (SList xs obj) = "(" ++ (showElements xs) ++ " . " ++ show obj ++ ")"
