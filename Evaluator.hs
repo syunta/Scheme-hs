af12cf081ab6837a4d27842512557c4be6236a4f
syunta <kitagawa@timedia.co.jp>
2017-03-23 21:06:37 +0900

    Add initialEnv

--- Evaluator.hs
+++ Evaluator.hs
@@ -2,6 +2,15 @@ import Types
 import Parser
 import Subr
 
+initialEnv :: Env
+initialEnv = extendEnv primitiveProcedureNames primitiveProcedureObjects []
+
+primitiveProcedureNames :: [String]
+primitiveProcedureNames = map fst primitiveProcedures
+
+primitiveProcedureObjects :: [SObj]
+primitiveProcedureObjects = map snd primitiveProcedures
+
 extendEnv :: [String] -> [SObj] -> Env -> Env
 extendEnv vars vals e = (vars, vals) : e
 
