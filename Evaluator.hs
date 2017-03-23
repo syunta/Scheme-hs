fd0434906a80086e03ab6b0ffad0f3f27f874546
syunta <kitagawa@timedia.co.jp>
2017-03-23 21:03:52 +0900

    Clean

--- Evaluator.hs
+++ Evaluator.hs
@@ -1,12 +1,6 @@
 import Types
 import Parser
+import Subr
 
 eval :: SObj -> Env -> SObj
-eval (SList (op:operands) _) env = primApply op operands -- application
 eval exp env = exp -- self evaluating
-
-primApply :: SObj -> [SObj] -> SObj
-primApply (Primitive op) operands = op operands
-
-primPlus :: [SObj] -> SObj -- TODO: monadic
-primPlus args = foldl (\(SInt x) (SInt acc) -> SInt (x + acc)) (SInt 0) args
