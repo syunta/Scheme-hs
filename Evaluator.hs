29037619dece50e58b762abcf1c3d5835fe9d9d4
syunta <kitagawa@timedia.co.jp>
2017-03-23 21:05:26 +0900

    Add extendEnv

--- Evaluator.hs
+++ Evaluator.hs
@@ -2,5 +2,8 @@ import Types
 import Parser
 import Subr
 
+extendEnv :: [String] -> [SObj] -> Env -> Env
+extendEnv vars vals e = (vars, vals) : e
+
 eval :: SObj -> Env -> SObj
 eval exp env = exp -- self evaluating
