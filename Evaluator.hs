808f02091f2b20f46df079ae75d4c1d44dffb011
syunta <kitagawa@timedia.co.jp>
2017-04-03 21:15:52 +0900

    Add simple if

--- Evaluator.hs
+++ Evaluator.hs
@@ -36,8 +36,17 @@ eval (SBool x) env = (SBool x)
 eval (SSymbol x) env = lookupEnv x env
 -- quotation
 eval (SList [SSymbol "quote", exp] _) env = exp
+-- if
+eval (SList ((SSymbol "if"):exps) _) env = evalIf (SList exps Nil) env
 -- application
 eval (SList (op:args) Nil) env = apply (eval op env) (map (flip eval env) args)
 
 apply :: SObj -> [SObj] -> SObj
 apply (Primitive x f) args = f args
+
+evalIf :: SObj -> Env -> SObj
+evalIf (SList [pred, cnsq] _) env =
+  let result = eval pred env in
+    case result of
+      (SBool False) -> (SBool False)
+      _             -> eval cnsq env
--- EvaluatorSpec.hs
+++ EvaluatorSpec.hs
@@ -15,6 +15,10 @@ main = hspec $ do
       eval (parse' "'10") initialEnv `shouldBe` parse' "10"
       eval (parse' "'(1 2 3)") initialEnv `shouldBe` parse' "(1 2 3)"
       eval (parse' "'hoge") initialEnv `shouldBe` parse' "hoge"
+    it "evaluates if syntax" $ do
+      eval (parse' "(if #f 3)") initialEnv `shouldBe` parse' "#f"
+      eval (parse' "(if #t 3)") initialEnv `shouldBe` parse' "3"
+      eval (parse' "(if 10 10)") initialEnv `shouldBe` parse' "10"
     it "evaluates primitive application" $ do
       eval (parse' "(+ 2 4)") initialEnv `shouldBe` parse' "6"
       eval (parse' "(+ 2 4 10)") initialEnv `shouldBe` parse' "16"
