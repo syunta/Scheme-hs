1a26c9c75031788248a6324789c0ece2e474cbc6
syunta <kitagawa@timedia.co.jp>
2017-04-05 21:22:22 +0900

    Add simple repl

--- Main.hs
+++ Main.hs
@@ -1,5 +1,18 @@
 import Parser
 import Evaluator
 
+run :: IO ()
+run = do
+  x <- readPrompt
+  let (expr, rest) = parse x
+      (val, env)   = eval (expr, initialEnv)
+  putStrLn (show val)
+  run
+
+readPrompt :: IO String
+readPrompt = do
+  putStr "scheme>"
+  getLine
+
 main :: IO ()
-main =
+main = run
