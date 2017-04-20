9d3e840656a3098a875406509c8448d24ba3142f
syunta <kitagawa@timedia.co.jp>
2017-04-20 21:43:56 +0900

    Add extendEnv

--- Env.hs
+++ Env.hs
@@ -8,3 +8,7 @@ import qualified Data.Map as M
 
 makeFrame :: [String] -> [SObj] -> Frame
 makeFrame vars vals = M.fromList $ zip vars vals
+
+extendEnv :: [String] -> [SObj] -> Env -> Env
+extendEnv vars vals (Node f m) = Node f $ M.insert num (Node (makeFrame vars vals) M.empty) m
+  where num = (1+) $ length . M.keys $ m
