b3e47f93a2dcefe87421a71cb600a36fba2ae325
syunta <kitagawa@timedia.co.jp>
2017-04-23 14:28:38 +0900

    Add lookupEnv

--- Env.hs
+++ Env.hs
@@ -20,5 +20,18 @@ makeFrame :: [String] -> [SObj] -> Frame
 makeFrame vars vals = M.fromList $ zip vars vals
 
 extendEnv :: [String] -> [SObj] -> Env -> Env
-extendEnv vars vals (Node f m) = Node f $ M.insert num (Node (makeFrame vars vals) M.empty) m
-  where num = (1+) $ length . M.keys $ m
+extendEnv vars vals (Node f e) = Node f $ M.insert num (Node (makeFrame vars vals) M.empty) e
+  where num = (1+) $ length . M.keys $ e
+
+lookupEnv :: String -> Env -> Ref -> Maybe SObj
+lookupEnv x (Node f e) [] = M.lookup x f
+lookupEnv x e (r:rs) = do
+  f <- scanEnv (Just e) (r:rs)
+  let val = M.lookup x f
+  case val of
+    Nothing -> lookupEnv x e rs
+    _       -> val
+
+scanEnv :: Maybe Env -> Ref -> Maybe Frame
+scanEnv (Just (Node f _)) []     = (Just f)
+scanEnv (Just (Node _ e)) (r:rs) = scanEnv (M.lookup r e) rs
