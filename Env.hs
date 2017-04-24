602849223f7a34e7ea0cfb6353ad4baa016aac8a
syunta <kitagawa@timedia.co.jp>
2017-04-24 20:19:50 +0900

    Add defineVar

--- Env.hs
+++ Env.hs
@@ -1,6 +1,6 @@
 module Env
 (
-  initialEnv, extendEnv, lookupEnv
+  initialEnv, extendEnv, lookupEnv, defineVar
 ) where
 
 import Types
@@ -24,14 +24,32 @@ extendEnv vars vals (Node f e) = Node f $ M.insert num (Node (makeFrame vars val
   where num = (1+) $ length . M.keys $ e
 
 lookupEnv :: String -> Env -> Ref -> Maybe SObj
-lookupEnv x (Node f e) [] = M.lookup x f
-lookupEnv x e (r:rs) = do
+lookupEnv var (Node f e) [] = M.lookup var f
+lookupEnv var e (r:rs) = do
   f <- scanEnv (Just e) (r:rs)
-  let val = M.lookup x f
+  let val = M.lookup var f
   case val of
-    Nothing -> lookupEnv x e rs
+    Nothing -> lookupEnv var e rs
     _       -> val
 
 scanEnv :: Maybe Env -> Ref -> Maybe Frame
 scanEnv (Just (Node f _)) []     = (Just f)
 scanEnv (Just (Node _ e)) (r:rs) = scanEnv (M.lookup r e) rs
+
+defineVar :: String -> SObj -> Env -> Ref -> Env
+defineVar var val e r = do
+  let f = scanEnv (Just e) r
+  case f of
+    Nothing -> e
+    (Just f) -> let newf = M.insert var val f in
+                replace newf e r
+
+replace :: Frame -> Env -> Ref -> Env
+replace f (Node _ e) [] = Node f e
+replace f (Node f' e) (r:rs) =
+  let e' = M.lookup r e in
+  case e' of
+    Nothing -> Node f' e
+    (Just e') ->
+      let newe = replace f e' rs in
+      Node f' (M.insert r newe e)
