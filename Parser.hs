487a07561699ea5367cf9e4786472993f44637d3
syunta <kitagawa@timedia.co.jp>
2017-08-16 18:48:18 +0900

    Refactor - Simplify parseList

--- Parser.hs
+++ Parser.hs
@@ -28,30 +28,18 @@ parseTokens ("'":ts) = parseQuote ts
 parseTokens (t:ts)   = (parseAtom t, ts)
 
 parseList :: (SObj, [String]) -> (SObj, [String])
-parseList (exps, ")":ts) = (exps, ts)
-parseList (exps, "'":ts) =
-  let (qls, restTokens) = parseQuote ts
-      (SList ls tail, restRestTokens) = parseList (exps, restTokens) in
-  (SList (qls : ls) tail, restRestTokens)
-parseList (exps, ".":ts) = parseDotList (exps, ts)
-parseList (exps, "(":ts) =
-  let (sls, restTokens) = parseList (SList [] Nil, ts)
-      (SList ls tail, restRestTokens) = parseList (exps, restTokens) in
-  (SList (sls : ls) tail, restRestTokens)
-parseList (exps, t:ts) =
-  let (SList ls tail, restTokens) = parseList (exps, ts) in
-  (SList (parseAtom t : ls) tail, restTokens)
+parseList (SList exps tail, ")":ts) = (SList (reverse exps) tail, ts)
+parseList (SList exps _,    ".":ts) =
+  let (tail, restTokens) = parseTokens ts in
+      parseList (SList exps tail, restTokens)
+parseList (SList exps tail, ts) =
+  let (exp, restTokens) = parseTokens ts in
+      parseList (SList (exp : exps) tail, restTokens)
 
 parseQuote :: [String] -> (SObj, [String])
 parseQuote ts =
   let (exps, restTokens) = parseTokens ts in
     (SList [SSymbol "quote", exps] Nil, restTokens)
-
-parseDotList :: (SObj, [String]) -> (SObj, [String])
-parseDotList (SList ls1 _, "(":ts) =
-  let (SList ls2 tail, ")":restTokens) = parseList (SList [] Nil, ts) in
-  (SList (ls1 ++ ls2) tail, restTokens)
-parseDotList (SList ls _, t:")":ts) = (SList ls (parseAtom t), ts)
 
 parseAtom :: String -> SObj
 parseAtom token
