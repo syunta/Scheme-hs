f777e5a7619115f5d3f8c65473a853ab7e4b8757
syunta <kitagawa@timedia.co.jp>
2017-09-09 16:42:33 +0900

    Support Monad.State for Eval - self evaluating value

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -6,113 +6,114 @@ module Evaluator
 import Types
 import Subr
 import Env
+import Control.Monad.State
 
 evl :: (SObj, Env) -> (SObj, Env)
-evl (exp, env) = eval (exp, env, [])
+evl (exp, env) = runState (eval exp []) env
 
-eval :: (SObj, Env, Ref) -> (SObj, Env)
+eval :: SObj -> Ref -> State Env SObj
 -- self evaluating
-eval (SInt x, env, r) = (SInt x, env)
-eval (SBool x, env, r) = (SBool x, env)
--- variable
-eval (SSymbol x, env, r) = do
-  let val = lookupEnv x env r
-  case val of
-    Nothing -> (Nil, env)
-    Just v  -> (v, env)
--- quotation
-eval (SList [SSymbol "quote", exp] _, env, r) = (exp, env)
--- assignment
-eval (SList (SSymbol "set!" : exps) _, env, r) = evalSet (exps, env, r)
--- definition
-eval (SList (SSymbol "define" : exps) _, env, r) = evalDef (exps, env, r)
--- if
-eval (SList (SSymbol "if" : exps) _, env, r) = evalIf (exps, env, r)
--- cond
-eval (SList (SSymbol "cond" : exps) _, env, r) = eval (cond2if $ SList (SSymbol "cond" : exps) Nil, env, r)
--- lambda
-eval (SList (SSymbol "lambda" : exps) _, env, r) = (makeLambda exps r, env)
--- begin
-eval (SList (SSymbol "begin" : exps) _, env, r) = evalSeq exps env r
--- application
-eval (SList (op:args) _, env, r) =
-  let (op', env')    = eval (op, env, r)
-      (args', env'') = evalArgs (args, env', r) in
-  apply op' args' env'' r
+eval (SInt x) r = return (SInt x)
+eval (SBool x) r = return (SBool x)
+---- variable
+--eval (SSymbol x, env, r) = do
+--  let val = lookupEnv x env r
+--  case val of
+--    Nothing -> (Nil, env)
+--    Just v  -> (v, env)
+---- quotation
+--eval (SList [SSymbol "quote", exp] _, env, r) = (exp, env)
+---- assignment
+--eval (SList (SSymbol "set!" : exps) _, env, r) = evalSet (exps, env, r)
+---- definition
+--eval (SList (SSymbol "define" : exps) _, env, r) = evalDef (exps, env, r)
+---- if
+--eval (SList (SSymbol "if" : exps) _, env, r) = evalIf (exps, env, r)
+---- cond
+--eval (SList (SSymbol "cond" : exps) _, env, r) = eval (cond2if $ SList (SSymbol "cond" : exps) Nil, env, r)
+---- lambda
+--eval (SList (SSymbol "lambda" : exps) _, env, r) = (makeLambda exps r, env)
+---- begin
+--eval (SList (SSymbol "begin" : exps) _, env, r) = evalSeq exps env r
+---- application
+--eval (SList (op:args) _, env, r) =
+--  let (op', env')    = eval (op, env, r)
+--      (args', env'') = evalArgs (args, env', r) in
+--  apply op' args' env'' r
+--
+--evalArgs :: ([SObj], Env, Ref) -> ([SObj], Env)
+--evalArgs (xs, env, r) = iter ([], env) xs
+--  where
+--    iter (ys, env) []         = (reverse ys, env)
+--    iter (ys, env) (exp:exps) =
+--      let (exp', env') = eval (exp, env, r) in
+--          iter (exp' : ys, env') exps
+--
+--apply :: SObj -> [SObj] -> Env -> Ref -> (SObj, Env)
+--apply (Primitive x) args env r = (getProc x primitiveProcedures args, env)
+--apply (SLambda ps "" body lr) args e r =
+--  let (v, e') = evalSeq body ee er in
+--    (v, e')
+--    where ee = extendEnv ps args e
+--          er = extendRef e lr
+--apply (SLambda ps p body lr) args e r =
+--  let (v, e') = evalSeq body ee er in
+--    (v, e')
+--    where ee = extendEnv (ps ++ [p]) (take (length ps) args ++ [SList (drop (length ps) args) Nil]) e
+--          er = extendRef e lr
+--
+--evalIf :: ([SObj], Env, Ref) -> (SObj, Env)
+--evalIf ([pred, cnsq], env, r) =
+--  let result = eval (pred, env, r) in
+--    case result of
+--      (SBool False, env) -> (SBool False, env)
+--      _                  -> eval (cnsq, env, r)
+--evalIf ([pred, cnsq, alt], env, r) =
+--  let result = eval (pred, env, r) in
+--    case result of
+--      (SBool False, env) -> eval (alt, env, r)
+--      _                  -> eval (cnsq, env, r)
+--
+--cond2if :: SObj -> SObj
+--cond2if (SList (SSymbol "cond" : clauses) _) = expandClause clauses
+--
+--expandClause :: [SObj] -> SObj
+--expandClause [] = SBool False
+--expandClause (SList (SSymbol "else" : clause) _ : _) = seq2begin clause
+--expandClause (SList (pred:cnsq) _ : clauses) = SList [SSymbol "if", pred, seq2begin cnsq, expandClause clauses] Nil
+--
+--seq2begin :: [SObj] -> SObj
+--seq2begin []   = Nil
+--seq2begin exps = SList (SSymbol "begin" : exps) Nil
+--
+--makeLambda :: Body -> Ref -> SObj
+--makeLambda (Nil:body) = SLambda [] "" body
+--makeLambda (SList exps Nil : body) = SLambda (makeParams exps) "" body
+--makeLambda (SList exps (SSymbol tail) : body) = SLambda (makeParams exps) tail body
+--
+--makeParams :: [SObj] -> [String]
+--makeParams [] = []
+--makeParams (SSymbol x : xs) = x : makeParams xs
+--
+--evalSeq :: [SObj] -> Env -> Ref -> (SObj, Env)
+--evalSeq xs env r = iter (Nil, env) xs
+--  -- iter :: (SObj, Env) -> [SObj] -> (SObj, Env)
+--  where
+--    iter x        []         = x
+--    iter (_, env) (exp:exps) = iter (eval (exp, env, r)) exps
+--
+--evalSet :: ([SObj], Env, Ref) -> (SObj, Env)
+--evalSet ([SSymbol var, val], env, r) =
+--  let (val', env') = eval (val, env, r) in
+--  (SSymbol "ok", setVar var val' env' r)
 
-evalArgs :: ([SObj], Env, Ref) -> ([SObj], Env)
-evalArgs (xs, env, r) = iter ([], env) xs
-  where
-    iter (ys, env) []         = (reverse ys, env)
-    iter (ys, env) (exp:exps) =
-      let (exp', env') = eval (exp, env, r) in
-          iter (exp' : ys, env') exps
-
-apply :: SObj -> [SObj] -> Env -> Ref -> (SObj, Env)
-apply (Primitive x) args env r = (getProc x primitiveProcedures args, env)
-apply (SLambda ps "" body lr) args e r =
-  let (v, e') = evalSeq body ee er in
-    (v, e')
-    where ee = extendEnv ps args e
-          er = extendRef e lr
-apply (SLambda ps p body lr) args e r =
-  let (v, e') = evalSeq body ee er in
-    (v, e')
-    where ee = extendEnv (ps ++ [p]) (take (length ps) args ++ [SList (drop (length ps) args) Nil]) e
-          er = extendRef e lr
-
-evalIf :: ([SObj], Env, Ref) -> (SObj, Env)
-evalIf ([pred, cnsq], env, r) =
-  let result = eval (pred, env, r) in
-    case result of
-      (SBool False, env) -> (SBool False, env)
-      _                  -> eval (cnsq, env, r)
-evalIf ([pred, cnsq, alt], env, r) =
-  let result = eval (pred, env, r) in
-    case result of
-      (SBool False, env) -> eval (alt, env, r)
-      _                  -> eval (cnsq, env, r)
-
-cond2if :: SObj -> SObj
-cond2if (SList (SSymbol "cond" : clauses) _) = expandClause clauses
-
-expandClause :: [SObj] -> SObj
-expandClause [] = SBool False
-expandClause (SList (SSymbol "else" : clause) _ : _) = seq2begin clause
-expandClause (SList (pred:cnsq) _ : clauses) = SList [SSymbol "if", pred, seq2begin cnsq, expandClause clauses] Nil
-
-seq2begin :: [SObj] -> SObj
-seq2begin []   = Nil
-seq2begin exps = SList (SSymbol "begin" : exps) Nil
-
-makeLambda :: Body -> Ref -> SObj
-makeLambda (Nil:body) = SLambda [] "" body
-makeLambda (SList exps Nil : body) = SLambda (makeParams exps) "" body
-makeLambda (SList exps (SSymbol tail) : body) = SLambda (makeParams exps) tail body
-
-makeParams :: [SObj] -> [String]
-makeParams [] = []
-makeParams (SSymbol x : xs) = x : makeParams xs
-
-evalSeq :: [SObj] -> Env -> Ref -> (SObj, Env)
-evalSeq xs env r = iter (Nil, env) xs
-  -- iter :: (SObj, Env) -> [SObj] -> (SObj, Env)
-  where
-    iter x        []         = x
-    iter (_, env) (exp:exps) = iter (eval (exp, env, r)) exps
-
-evalSet :: ([SObj], Env, Ref) -> (SObj, Env)
-evalSet ([SSymbol var, val], env, r) =
-  let (val', env') = eval (val, env, r) in
-  (SSymbol "ok", setVar var val' env' r)
-
-evalDef :: ([SObj], Env, Ref) -> (SObj, Env)
-evalDef ([SSymbol var, SList (SSymbol "lambda" : body) _], env, r) =
-  (SSymbol "ok", defineVar var (makeLambda body r) env r)
-evalDef (SList (SSymbol var : params) Nil : body, env, r) =
-  (SSymbol "ok", defineVar var (makeLambda (SList params Nil : body) r) env r)
-evalDef (SList (SSymbol var : params) tail : body, env, r) =
-  (SSymbol "ok", defineVar var (makeLambda (SList params tail : body) r) env r)
-evalDef ([SSymbol var, val], env, r) =
-  let (val', env') = eval (val, env, r) in
-  (SSymbol "ok", defineVar var val' env' r)
+--evalDef :: ([SObj], Env, Ref) -> (SObj, Env)
+--evalDef ([SSymbol var, SList (SSymbol "lambda" : body) _], env, r) =
+--  (SSymbol "ok", defineVar var (makeLambda body r) env r)
+--evalDef (SList (SSymbol var : params) Nil : body, env, r) =
+--  (SSymbol "ok", defineVar var (makeLambda (SList params Nil : body) r) env r)
+--evalDef (SList (SSymbol var : params) tail : body, env, r) =
+--  (SSymbol "ok", defineVar var (makeLambda (SList params tail : body) r) env r)
+--evalDef ([SSymbol var, val], env, r) =
+--  let (val', env') = eval (val, env, r) in
+--  (SSymbol "ok", defineVar var val' env' r)
--- test/EvaluatorSpec.hs
+++ test/EvaluatorSpec.hs
@@ -17,46 +17,46 @@ spec = do
   describe "eval" $ do
     it "evaluates self-evaluating-value" $ do
       eval' (parse' "10", initialEnv) `shouldBe` parse' "10"
-    it "evaluates quoted expression" $ do
-      eval' (parse' "'10", initialEnv) `shouldBe` parse' "10"
-      eval' (parse' "'(1 2 3)", initialEnv) `shouldBe` parse' "(1 2 3)"
-      eval' (parse' "'hoge", initialEnv) `shouldBe` parse' "hoge"
-    it "evaluates set! syntax" $ do
-      eval' (parse' "(begin (define x 1) (set! x 2) x)", initialEnv) `shouldBe` parse' "2"
-      eval' (parse' "(begin (define x 1) (set! x (+ x 1)) x)", initialEnv) `shouldBe` parse' "2"
-      eval' (parse' "(begin (define c 0) (define (plus!) (set! c (+ c 1)) +) (define (one!) (set! c (+ c 1)) 1) ((plus!) (one!) (one!)) c)", initialEnv) `shouldBe` parse' "3"
-    it "evaluates define syntax" $ do
-      eval' (parse' "(begin (define hoge 20) hoge)", initialEnv) `shouldBe` parse' "20"
-      eval' (parse' "(begin (define hoge 20) (define hoge 10) hoge)", initialEnv) `shouldBe` parse' "10"
-      eval' (parse' "(begin (define hoge (+ 9 1)) hoge)", initialEnv) `shouldBe` parse' "10"
-    it "evaluates if syntax" $ do
-      eval' (parse' "(if #f 3)", initialEnv) `shouldBe` parse' "#f"
-      eval' (parse' "(if #t 3)", initialEnv) `shouldBe` parse' "3"
-      eval' (parse' "(if 10 10)", initialEnv) `shouldBe` parse' "10"
-      eval' (parse' "(if #f 1 0)", initialEnv) `shouldBe` parse' "0"
-      eval' (parse' "(if #t 1 0)", initialEnv) `shouldBe` parse' "1"
-    it "evaluates cond syntax" $ do
-      eval' (parse' "(cond ((= 0 1) 0) (else 1))", initialEnv) `shouldBe` parse' "1"
-      eval' (parse' "(cond ((= 0 1) 0))", initialEnv) `shouldBe` parse' "#f"
-      eval' (parse' "(cond ((= 0 1) 0) ((= 1 1) 0 (+ 2 1)) (else 9))", initialEnv) `shouldBe` parse' "3"
-    it "evaluates lambda syntax" $ do
-      eval' (parse' "(lambda () 1 2)", initialEnv) `shouldBe` SLambda [] "" [SInt 1, SInt 2] []
-      eval' (parse' "(lambda (x) 1)", initialEnv) `shouldBe` SLambda ["x"] "" [SInt 1] []
-      eval' (parse' "(lambda (x . args) 1)", initialEnv) `shouldBe` SLambda ["x"] "args" [SInt 1] []
-    it "evaluates begin syntax" $ do
-      eval' (parse' "(begin 1 2 3 4 5)", initialEnv) `shouldBe` parse' "5"
-    it "evaluates primitive procedure" $ do
-      eval' (parse' "(+ 2 4)", initialEnv) `shouldBe` parse' "6"
-      eval' (parse' "(+ 2 4 10)", initialEnv) `shouldBe` parse' "16"
-      eval' (parse' "(- 6 4)", initialEnv) `shouldBe` parse' "2"
-      eval' (parse' "(- 10 3 2)", initialEnv) `shouldBe` parse' "5"
-      eval' (parse' "(* 2 3 5)", initialEnv) `shouldBe` parse' "30"
-      eval' (parse' "(/ 10 2)", initialEnv) `shouldBe` parse' "5"
-    it "evaluates compound procedure" $ do
-      eval' (parse' "((lambda (x y) (+ x y)) 3 5)", initialEnv) `shouldBe` parse' "8"
-      eval' (parse' "(begin (define plus (lambda (x y) (+ x y))) (plus 3 5))", initialEnv) `shouldBe` parse' "8"
-      eval' (parse' "(begin (define (plus x y) (+ x y)) (plus 2 5))", initialEnv) `shouldBe` parse' "7"
-      eval' (parse' "(begin (define (tail x . args) args) (tail 2 5 9))", initialEnv) `shouldBe` parse' "(5 9)"
-      eval'
-        (parse' "(begin (define (fib n) (if (= n 0) 0 (if (= n 1) 1 (+ (fib (- n 1)) (fib (- n 2)))))) (fib 10))", initialEnv)
-        `shouldBe` parse' "55"
+--    it "evaluates quoted expression" $ do
+--      eval' (parse' "'10", initialEnv) `shouldBe` parse' "10"
+--      eval' (parse' "'(1 2 3)", initialEnv) `shouldBe` parse' "(1 2 3)"
+--      eval' (parse' "'hoge", initialEnv) `shouldBe` parse' "hoge"
+--    it "evaluates set! syntax" $ do
+--      eval' (parse' "(begin (define x 1) (set! x 2) x)", initialEnv) `shouldBe` parse' "2"
+--      eval' (parse' "(begin (define x 1) (set! x (+ x 1)) x)", initialEnv) `shouldBe` parse' "2"
+--      eval' (parse' "(begin (define c 0) (define (plus!) (set! c (+ c 1)) +) (define (one!) (set! c (+ c 1)) 1) ((plus!) (one!) (one!)) c)", initialEnv) `shouldBe` parse' "3"
+--    it "evaluates define syntax" $ do
+--      eval' (parse' "(begin (define hoge 20) hoge)", initialEnv) `shouldBe` parse' "20"
+--      eval' (parse' "(begin (define hoge 20) (define hoge 10) hoge)", initialEnv) `shouldBe` parse' "10"
+--      eval' (parse' "(begin (define hoge (+ 9 1)) hoge)", initialEnv) `shouldBe` parse' "10"
+--    it "evaluates if syntax" $ do
+--      eval' (parse' "(if #f 3)", initialEnv) `shouldBe` parse' "#f"
+--      eval' (parse' "(if #t 3)", initialEnv) `shouldBe` parse' "3"
+--      eval' (parse' "(if 10 10)", initialEnv) `shouldBe` parse' "10"
+--      eval' (parse' "(if #f 1 0)", initialEnv) `shouldBe` parse' "0"
+--      eval' (parse' "(if #t 1 0)", initialEnv) `shouldBe` parse' "1"
+--    it "evaluates cond syntax" $ do
+--      eval' (parse' "(cond ((= 0 1) 0) (else 1))", initialEnv) `shouldBe` parse' "1"
+--      eval' (parse' "(cond ((= 0 1) 0))", initialEnv) `shouldBe` parse' "#f"
+--      eval' (parse' "(cond ((= 0 1) 0) ((= 1 1) 0 (+ 2 1)) (else 9))", initialEnv) `shouldBe` parse' "3"
+--    it "evaluates lambda syntax" $ do
+--      eval' (parse' "(lambda () 1 2)", initialEnv) `shouldBe` SLambda [] "" [SInt 1, SInt 2] []
+--      eval' (parse' "(lambda (x) 1)", initialEnv) `shouldBe` SLambda ["x"] "" [SInt 1] []
+--      eval' (parse' "(lambda (x . args) 1)", initialEnv) `shouldBe` SLambda ["x"] "args" [SInt 1] []
+--    it "evaluates begin syntax" $ do
+--      eval' (parse' "(begin 1 2 3 4 5)", initialEnv) `shouldBe` parse' "5"
+--    it "evaluates primitive procedure" $ do
+--      eval' (parse' "(+ 2 4)", initialEnv) `shouldBe` parse' "6"
+--      eval' (parse' "(+ 2 4 10)", initialEnv) `shouldBe` parse' "16"
+--      eval' (parse' "(- 6 4)", initialEnv) `shouldBe` parse' "2"
+--      eval' (parse' "(- 10 3 2)", initialEnv) `shouldBe` parse' "5"
+--      eval' (parse' "(* 2 3 5)", initialEnv) `shouldBe` parse' "30"
+--      eval' (parse' "(/ 10 2)", initialEnv) `shouldBe` parse' "5"
+--    it "evaluates compound procedure" $ do
+--      eval' (parse' "((lambda (x y) (+ x y)) 3 5)", initialEnv) `shouldBe` parse' "8"
+--      eval' (parse' "(begin (define plus (lambda (x y) (+ x y))) (plus 3 5))", initialEnv) `shouldBe` parse' "8"
+--      eval' (parse' "(begin (define (plus x y) (+ x y)) (plus 2 5))", initialEnv) `shouldBe` parse' "7"
+--      eval' (parse' "(begin (define (tail x . args) args) (tail 2 5 9))", initialEnv) `shouldBe` parse' "(5 9)"
+--      eval'
+--        (parse' "(begin (define (fib n) (if (= n 0) 0 (if (= n 1) 1 (+ (fib (- n 1)) (fib (- n 2)))))) (fib 10))", initialEnv)
+--        `shouldBe` parse' "55"
