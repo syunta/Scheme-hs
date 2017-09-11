b0866686c4e42e3ab8f21e5d2d212b658ebb3119
syunta <kitagawa@timedia.co.jp>
2017-09-12 01:34:37 +0900

    Contain Ref in State

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -9,73 +9,80 @@ import Env
 import Control.Monad.State
 
 evl :: SObj -> Env -> (SObj, Env)
-evl exp env = runState (eval exp []) env
+evl exp env = let (val, (env', _)) = runState (eval exp) $ (env, []) in
+                (val, env')
 
-eval :: SObj -> Ref -> State Env SObj
+eval :: SObj -> State (Env, Ref) SObj
 -- self evaluating
-eval (SInt x) r = return $ SInt x
-eval (SBool x) r = return $ SBool x
+eval (SInt x) = return $ SInt x
+eval (SBool x) = return $ SBool x
 -- variable
-eval (SSymbol x) r = do
-  env <- get
+eval (SSymbol x) = do
+  (env, r) <- get
   let val = lookupEnv x env r
   case val of
     Nothing -> return Nil
     Just v  -> return v
 -- quotation
-eval (SList [SSymbol "quote", exp] _) r = return exp
+eval (SList [SSymbol "quote", exp] _) = return exp
 -- assignment
-eval (SList (SSymbol "set!" : exps) _) r = evalSet exps r
+eval (SList (SSymbol "set!" : exps) _) = evalSet exps
 -- definition
-eval (SList (SSymbol "define" : exps) _) r = evalDef exps r
+eval (SList (SSymbol "define" : exps) _) = evalDef exps
 ---- if
-eval (SList (SSymbol "if" : exps) _) r = evalIf exps r
+eval (SList (SSymbol "if" : exps) _) = evalIf exps
 -- cond
-eval (SList (SSymbol "cond" : exps) _) r = eval (cond2if $ SList (SSymbol "cond" : exps) Nil) r
+eval (SList (SSymbol "cond" : exps) _) = eval (cond2if $ SList (SSymbol "cond" : exps) Nil)
 -- lambda
-eval (SList (SSymbol "lambda" : exps) _) r = return $ makeLambda exps r
+eval (SList (SSymbol "lambda" : exps) _) = do
+  (_, r) <- get
+  return $ makeLambda exps r
 -- begin
-eval (SList (SSymbol "begin" : exps) _) r = evalSeq exps r
+eval (SList (SSymbol "begin" : exps) _) = evalSeq exps
 -- application
-eval (SList (op:args) _) r = do
-  op' <- eval op r
-  args' <- evalArgs args r
-  apply op' args' r
+eval (SList (op:args) _) = do
+  op' <- eval op
+  args' <- evalArgs args
+  apply op' args'
 
-evalArgs :: [SObj] -> Ref -> State Env [SObj]
-evalArgs exps r = mapM (flip eval $ r) exps
+evalArgs :: [SObj] -> State (Env, Ref) [SObj]
+evalArgs exps = mapM eval exps
 
-apply :: SObj -> [SObj] -> Ref -> State Env SObj
-apply (Primitive x) args r = do
+apply :: SObj -> [SObj] -> State (Env, Ref) SObj
+apply (Primitive x) args = do
   let result = lookupPrimitive (Primitive x)
   case result of
     Nothing -> return Nil
     Just p  -> return (p args)
-apply (SLambda params "" body lr) args r = do
-  env <- get
-  put $ extendEnv params args env
-  env' <- get
-  val <- evalSeq body (extendRef env lr)
+apply (SLambda params "" body lr) args = do
+  (env, r) <- get
+  put $ (extendEnv params args env, extendRef env lr)
+  val <- evalSeq body
+  (env', _) <- get
+  put (env', r)
   return val
-apply (SLambda params p body lr) args r = do
-  env <- get
+apply (SLambda params p body lr) args = do
+  (env, r) <- get
   let n = length params
-  put $ extendEnv (params ++ [p]) (take n args ++ [SList (drop n args) Nil]) env
-  env' <- get
-  val <- evalSeq body (extendRef env lr)
+  let newEnv = extendEnv (params ++ [p]) (take n args ++ [SList (drop n args) Nil]) env
+  let newRef = extendRef env lr
+  put $ (newEnv, newRef)
+  val <- evalSeq body
+  (env', _) <- get
+  put (env', r)
   return val
 
-evalIf :: [SObj] -> Ref -> State Env SObj
-evalIf [pred, cnsq] r = do
-  result <- eval pred r
+evalIf :: [SObj] -> State (Env, Ref) SObj
+evalIf [pred, cnsq] = do
+  result <- eval pred
   case result of
     SBool False -> return $ SBool False
-    _           -> eval cnsq r
-evalIf [pred, cnsq, alt] r = do
-  result <- eval pred r
+    _           -> eval cnsq
+evalIf [pred, cnsq, alt] = do
+  result <- eval pred
   case result of
-    SBool False -> eval alt r
-    _           -> eval cnsq r
+    SBool False -> eval alt
+    _           -> eval cnsq
 
 cond2if :: SObj -> SObj
 cond2if (SList (SSymbol "cond" : clauses) _) = expandClause clauses
@@ -97,31 +104,31 @@ makeLambda (SList exps (SSymbol tail) : body) = SLambda (makeParams exps) tail b
 makeParams :: [SObj] -> [String]
 makeParams exps = map (\(SSymbol name) -> name) exps
 
-evalSeq :: [SObj] -> Ref -> State Env SObj
-evalSeq exps r = foldM (\x y ->  eval y r) Nil exps
+evalSeq :: [SObj] -> State (Env, Ref) SObj
+evalSeq exps = foldM (\x y ->  eval y) Nil exps
 
-evalSet :: [SObj] -> Ref -> State Env SObj
-evalSet [SSymbol var, val] r = do
-  val' <- eval val r
-  env <- get
-  put $ setVar var val' env r
+evalSet :: [SObj] -> State (Env, Ref) SObj
+evalSet [SSymbol var, val] = do
+  val' <- eval val
+  (env, r) <- get
+  put $ (setVar var val' env r, r)
   return $ SSymbol "ok"
 
-evalDef :: [SObj] -> Ref -> State Env SObj
-evalDef [SSymbol var, SList (SSymbol "lambda" : body) _] r = do
-  env <- get
-  put $ defineVar var (makeLambda body r) env r
+evalDef :: [SObj] -> State (Env, Ref) SObj
+evalDef [SSymbol var, SList (SSymbol "lambda" : body) _] = do
+  (env, r) <- get
+  put $ (defineVar var (makeLambda body r) env r, r)
   return $ SSymbol "ok"
-evalDef (SList (SSymbol var : params) Nil : body) r = do
-  env <- get
-  put $ defineVar var (makeLambda (SList params Nil : body) r) env r
+evalDef (SList (SSymbol var : params) Nil : body) = do
+  (env, r) <- get
+  put $ (defineVar var (makeLambda (SList params Nil : body) r) env r, r)
   return $ SSymbol "ok"
-evalDef (SList (SSymbol var : params) tail : body) r = do
-  env <- get
-  put $ defineVar var (makeLambda (SList params tail : body) r) env r
+evalDef (SList (SSymbol var : params) tail : body) = do
+  (env, r) <- get
+  put $ (defineVar var (makeLambda (SList params tail : body) r) env r, r)
   return $ SSymbol "ok"
-evalDef [SSymbol var, val] r = do
-  val' <- eval val r
-  env <- get
-  put $ defineVar var val' env r
+evalDef [SSymbol var, val] = do
+  val' <- eval val
+  (env, r) <- get
+  put $ (defineVar var val' env r, r)
   return $ SSymbol "ok"
